4be942be03e7f500dcb7af2e8002a96b0e9b39bc
sensspangerankegelapan <ntnsendi321@gmail.com>
2026-06-28 16:54:52 +0700

    Update server.js

--- server.js
+++ server.js
@@ -1,16 +1,15 @@
 const express = require("express");
 
 const app = express();
+const PORT = process.env.PORT || 3000;
 
 app.get("/", (req, res) => {
   res.json({
     status: true,
-    message: "Video Render API Running"
+    message: "Backend Video Render Aktif"
   });
 });
 
-const PORT = process.env.PORT || 3000;
-
 app.listen(PORT, () => {
-  console.log("Server running on port", PORT);
+  console.log(`Server berjalan di port ${PORT}`);
 });
