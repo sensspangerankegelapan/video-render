a1ef10d774f96a821306dfb6320d1f740a1630d9
sensspangerankegelapan <ntnsendi321@gmail.com>
2026-06-28 16:36:45 +0700

    Create server.js

---
+++ server.js
@@ -0,0 +1,16 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.json({
+    status: true,
+    message: "Video Render API Running"
+  });
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+  console.log("Server running on port", PORT);
+});
