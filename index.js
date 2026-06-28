636eb6dd7f721fe792e2632ef67484b76c45a762
sensspangerankegelapan <ntnsendi321@gmail.com>
2026-06-28 16:58:49 +0700

    Update index.js

--- index.js
+++ index.js
@@ -1,18 +1,5 @@
-const express = require("express")
-
-const app = express()
-
-app.use(express.json())
-
-app.get("/", (req, res) => {
-  res.json({
-    status: true,
-    message: "Video Render API Online"
-  })
-})
-
-const PORT = process.env.PORT || 3000
-
-app.listen(PORT, () => {
-  console.log("Server running on port " + PORT)
-})
+module.exports = {
+  status: true,
+  author: "sensspangerankegelapan",
+  message: "Backend Video Render Ready"
+};
